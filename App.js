a0703f34ef81b4be3a41d79a84ffec571351330b
Muhammad Saad Atique <msaadatiq@gmail.com>
2022-08-14 17:58:41 -0400

    added background to the app

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import bg from './assets/bg.jpeg';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={bg} style={styles.bg}>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  bg: {
+    width: "100%",
+    height: "100%",
   },
 });
